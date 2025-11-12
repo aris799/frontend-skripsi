749a6ed756d74cf867aaffb8e813728241961f85
aris799 <106787886+aris799@users.noreply.github.com>
2025-11-12 10:38:44 +0700

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
+import "../styles/Landingpage.css";
 import { Link } from 'react-router-dom';
-import styles from './Landing.module.css';
 
 const Landing = () => {
   return (
-    <div className={styles.landingMain}>
+    <div className='landing-main'>
       <img 
         src="https://res.cloudinary.com/diogvlobw/image/upload/v1762062126/Logo_SRSIK_Horizontal-x_vehgyz.png" 
         alt="Logo SRSIK" 
-        className={styles.landingLogo}
+        className="landing-logo" 
       />
-      <h1 className={styles.landingTitle}>Mulai Skripsi Lebih Mudah</h1>
-      <p className={styles.landingDescription}>
-        Bangun skripsi yang lebih terarah dengan rekomendasi cerdas dari SRSIK Hub. <br /> 
-        Masuk atau daftar untuk memulai perjalanan akademik Anda.
-      </p>
-      <Link 
-        to="/login" 
-        className={styles.landingButton}
-      >
-        Login
-      </Link>
-      <Link 
-        to="/register" 
-        className={styles.landingButton}
-      >
-        Register
-      </Link>
+      <h1>Mulai Skripsi Lebih Mudah</h1>
+      <p>Bangun skripsi yang lebih terarah dengan rekomendasi cerdas dari SRSIK Hub. <br /> Masuk atau daftar untuk memulai perjalanan akademik Anda.</p>
+      <Link to="/login" className="landing-login-button">Login</Link>
+      <Link to="/register" className="landing-register-button">Register</Link>
     </div>
   );
 }
